3fbc82bec00063dd50900defc923ae2a53dfc4b2
Ali Zain <alizain10166@gmail.com>
2023-08-14 14:21:14 +0200

    Functionality added and fixed height for Task-list

--- v1.0.0-beta/components/main.js
+++ v1.0.0-beta/components/main.js
@@ -49,3 +49,93 @@ navLinks.forEach((link) => {
 
   navItems.appendChild(navUl);
 });
+
+// <!-- Task-List -->
+
+const ulTask = document.querySelector(".ul-Task");
+const taskName = document.querySelector("#task-name");
+const priority = document.querySelector("#priority");
+const category = document.querySelector("#category");
+const dueDate = document.querySelector("#due-date");
+const btn = document.querySelector("#submit-btn");
+let counter = 1;
+
+const clickHandler = (e) => {
+  e.preventDefault();
+  // -----------------------------------------
+  if (
+    taskName.value === "" ||
+    priority.value === "" ||
+    category.value === "" ||
+    dueDate.value === ""
+  ) {
+    alert("Please fill in all fields before submitting.");
+    return;
+  }
+
+  const liTask = document.createElement("li");
+  liTask.classList.add("li-Task");
+
+  // ChekBox
+  const chekBox = document.createElement("input");
+  chekBox.setAttribute("type", "checkbox");
+  chekBox.id = "chekbox";
+
+  // Name
+  const inputName = document.createElement("h3");
+  inputName.textContent = taskName.value;
+
+  // Priority
+  const priorityDisplay = document.createElement("p");
+  priorityDisplay.textContent = priority.value;
+
+  // Category
+  const categoryDisplay = document.createElement("p");
+  categoryDisplay.textContent = category.value;
+
+  // Due-date
+  const dateDisplay = document.createElement("p");
+  dateDisplay.textContent = dueDate.value;
+
+  // ------------------------------------
+  // Delete-Btn
+  const dltBtn = document.createElement("button");
+  dltBtn.textContent = "Delete";
+  dltBtn.classList = "dltbtn";
+
+  dltBtn.addEventListener("click", () => {
+    ulTask.removeChild(liTask);
+  });
+
+  // chekBox
+  chekBox.addEventListener("click", () => {
+    if (liTask.style.textDecoration === "line-through") {
+      liTask.style.textDecoration = "none";
+    } else {
+      liTask.style.textDecoration = "line-through";
+    }
+  });
+
+  // --------------------------------------
+  if (counter == 6) {
+    ulTask.removeChild(liTask);
+    return;
+  }
+  counter++;
+
+  // ---------------------------------------
+  liTask.appendChild(chekBox);
+  liTask.appendChild(inputName);
+  liTask.appendChild(priorityDisplay);
+  liTask.appendChild(categoryDisplay);
+  liTask.appendChild(dateDisplay);
+  liTask.appendChild(dltBtn);
+  ulTask.appendChild(liTask);
+  // -----------------------------------------
+  // taskName.value = "";
+  // priority.value = "";
+  // category.value = "";
+  // dueDate.value = "";
+};
+
+btn.addEventListener("click", clickHandler);
